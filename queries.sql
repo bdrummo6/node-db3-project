83a5166c489d1f090ae5b8064eb7955fc8900562
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-18 12:48:25 -0400

    Completed First Stretch Problem

--- queries.sql
+++ queries.sql
@@ -7,8 +7,13 @@ SELECT ProductName, CategoryName FROM Product JOIN Category ON Product.CategoryI
 SELECT "Order".Id, CompanyName FROM "Order" JOIN Shipper ON "Order".ShipVia = Shipper.Id WHERE OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-SELECT ProductName, Quantity FROM OrderDetail JOIN Product ON Product.Id = OrderDetail.ProductId WHERE OrderDetail.OrderId = '10251' ORDER BY ProductName;
+SELECT ProductName, Quantity FROM OrderDetail JOIN Product ON Product.Id = OrderDetail.ProductId WHERE OrderDetail.OrderId = '10251' 
+ORDER BY ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 SELECT "Order".Id AS "OrderID", CompanyName AS "Customer's Company Name", LastName AS "Employee's Last Name" FROM "Order"
 JOIN Customer ON "Order".CustomerId = Customer.Id JOIN Employee ON "Order".EmployeeId = Employee.id;
+
+-- (STRETCH) Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 8 records.
+SELECT CategoryName, Count(ProductName) AS "Count" FROM Products JOIN Categories ON Products.CategoryID = Categories.CategoryID 
+GROUP BY CategoryName;

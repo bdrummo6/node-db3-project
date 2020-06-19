67a4887468a7fc3a5d0228c866d55dee1f588fda
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-19 16:43:28 -0400

    Completed last two stretch items
    Completed the addSteps method and tested it using the given endpoint.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,24 +5,25 @@ module.exports = {
     find,
     findById, 
     findSteps,
-    add, 
+    add,
+    addStep,
     update, 
     remove
 }
 
 // Returns all scheme objects
-function find() {
+async function find() {
     return db('schemes');
 }
 
 // Returns the scheme object with the specified id
-function findById(id) {
+async function findById(id) {
     // first() returns the first entry in the db matching the query
     return db('schemes').where({ id }).first();
 }
 
 // Returns the steps in order for the scheme with the specified id
-function findSteps(id) {
+async function findSteps(id) {
     return db('steps').join('schemes', 'steps.scheme_id', '=', 'schemes.id')
                       .select('steps.id', 'scheme_name', 'step_number', 'instructions')
                       .where('schemes.id', id)
@@ -36,6 +37,13 @@ async function add(scheme) {
     return db('schemes').where({ id }).first();
 }
 
+// (Stretch) Adds a step to a scheme
+async function addStep(step, scheme_id) {
+    const [id] = await db('steps').insert({ ...step, scheme_id: scheme_id })
+
+    return db('steps').where({ id }).first();
+}
+
 
 // Updates a scheme object with the specified id
 async function update(changes, id) {
@@ -45,8 +53,8 @@ async function update(changes, id) {
 }
 
 // Removes the scheme object with the specified id
-function remove(id) {
-    return db('schemes').where({ id }).del();
+async function remove(id) {
+    return await db('schemes').where({ id }).del();
 }
 
 
